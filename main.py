be3aafcfbc4dadbb63e358f20fd062e8047b7fbd
unknown <dartx7x@gmail.com>
2023-02-26 11:26:54 +0400

    Read node_coords file into dict

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-with open
+nodes = dict()
+with open("node_coords.txt", mode="r") as file:
+    for line in file:
+        values = line.split(",")
+        nodes[int(values[0])] = { "nx": float(values[1]), "ny": float(values[2]) }
