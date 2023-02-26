62386421e3723a7d1aa9f13c42163f210528f47d
unknown <dartx7x@gmail.com>
2023-02-26 11:57:44 +0400

    Read elem_nodes in dict

--- main.py
+++ main.py
@@ -3,3 +3,20 @@ with open("node_coords.txt", mode="r") as file:
     for line in file:
         values = line.split(",")
         nodes[int(values[0])] = { "nx": float(values[1]), "ny": float(values[2]) }
+
+elems = dict()
+elem_num = 0
+with open("elem_nodes.txt", mode="r") as file:
+    while True:
+        num_nodes_per_elem = file.readline()
+        if num_nodes_per_elem == "":
+            break
+        elem_num += 1
+        elem_nodes = list()
+        for i in range(int(num_nodes_per_elem)):
+            node = file.readline()
+            elem_nodes.append(int(node))
+        elems[elem_num] = { "nodes": elem_nodes }
+
+
+print(elems[1251])
