d6618d18ccc3f2832ae38390a2ad6fa8773c4a07
unknown <dartx7x@gmail.com>
2023-03-04 10:27:30 +0400

    Add matrix related functions and vars

--- helpers.py
+++ helpers.py
@@ -1,3 +1,5 @@
+import math
+
 def max_coord(nodes, coord):
     max = nodes[1][coord]
     for node in nodes:
@@ -5,10 +7,26 @@ def max_coord(nodes, coord):
             max = nodes[node][coord]
     return max
 
-
 def min_coord(nodes, coord):
     min = nodes[1][coord]
     for node in nodes:
         if nodes[node][coord] < min:
             min = nodes[node][coord]
     return min
+
+
+def get_num_x_tiles():
+    return 7
+
+
+def get_num_y_tiles():
+    return 5
+
+
+def get_step(min_coord, max_coord, num_tiles):
+    return (abs(min_coord) + abs(max_coord)) / num_tiles
+
+
+def get_index(min_coord, coord, step):
+    index = math.ceil((abs(min_coord) + coord) / step) - 1
+    return index if index >= 0 else 0
--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+import numpy
+
 from src_readers import get_node_coords, get_elems
 from helpers import *
 
 nodes = get_node_coords()
 elems = get_elems()
+
+
+min_x = min_coord(nodes, "x")
+max_x = max_coord(nodes, "x")
+min_y = min_coord(nodes, "y")
+max_y = max_coord(nodes, "y")
+
+num_x_tiles = get_num_x_tiles()
+num_y_tiles = get_num_y_tiles()
+
+x_step = get_step(min_x, max_x, num_x_tiles)
+y_step = get_step(min_y, max_y, num_y_tiles)
+
+matrix = numpy.zeros((num_y_tiles, num_x_tiles))
+
+for i in range(1, max(elems)):
+    coords = elems[i]["centroid_coords"]
+    matrix[get_index(min_y, coords["y"], y_step)][get_index(min_x, coords["x"], x_step)] += 1
+
+print(matrix)
