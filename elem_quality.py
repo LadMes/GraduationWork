d58aad7f6c08e8d1dcf4b9611ce0b979fd3cebf7
unknown <dartx7x@gmail.com>
2023-05-23 12:06:51 +0400

    Add all quality metric checks

--- elem_quality.py
+++ elem_quality.py
@@ -1,5 +1,5 @@
 def is_bad_elem(elem):
-    return is_bad_elem_by_ASPE(elem) or is_bad_elem_by_JACR(elem)
+    return is_bad_elem_by_ASPE(elem) or is_bad_elem_by_JACR(elem) or is_bad_elem_by_MAXA(elem) or is_bad_elem_by_PARA(elem) or is_bad_elem_by_WARP(elem)
 
 
 def is_bad_elem_by_ASPE(elem):
@@ -16,3 +16,29 @@ def is_bad_elem_by_JACR(elem):
         return False
     
     return True
+
+
+def is_bad_elem_by_MAXA(elem):
+
+    if elem["elem_shape_prop"]["MAXA"] >= 90 and elem["elem_shape_prop"]["MAXA"] <= 135:
+        return False
+    
+    return True
+
+
+def is_bad_elem_by_PARA(elem):
+
+    if elem["elem_shape_prop"]["PARA"] >= 0 and elem["elem_shape_prop"]["PARA"] <= 50:
+        return False
+    
+    return True
+
+
+def is_bad_elem_by_WARP(elem):
+    if "WARP" in elem["elem_shape_prop"]:
+        if elem["elem_shape_prop"]["WARP"] >= 0 and elem["elem_shape_prop"]["WARP"] <= 1:
+            return False
+        
+        return True
+    
+    return False
